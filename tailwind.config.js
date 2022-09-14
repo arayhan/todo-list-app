541d37164d3081da6511d8d5f77cbec691f91127
Rayhan <rayhanprima99@gmail.com>
2022-09-15 01:53:47 +0700

    chore: implement tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			container: {
+				center: true,
+				padding: '1rem',
+			},
+		},
 	},
 	plugins: [],
 };
