8b1310910fc5852bc0c3bdda007fe24df4414e61
Rayhan <rayhanprima99@gmail.com>
2022-09-14 22:31:37 +0700

    chore: initialization
    - add config for absolute import
    - update folder & file structure

--- config-overrides.js
+++ config-overrides.js
@@ -2,15 +2,13 @@ const { alias } = require('react-app-rewire-alias');
 
 module.exports = function override(config) {
 	alias({
-		'@components': './src/components',
-		'@config': './src/config',
-		'@images': './src/images',
-		'@pages': './src/pages',
-		'@services': './src/services',
-		'@store': './src/store',
-		'@styles': './src/styles',
-		'@tests': './src/tests',
-		'@utils': './src/utils',
+		'@components': './src/components/',
+		'@images': './src/images/',
+		'@pages': './src/pages/',
+		'@services': './src/services/',
+		'@styles': './src/styles/',
+		'@tests': './src/tests/',
+		'@utils': './src/utils/',
 	})(config);
 
 	return config;
