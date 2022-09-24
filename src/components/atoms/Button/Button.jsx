7d89b31725ac353b38655c6c7adffe37c636020d
Rayhan <rayhanprima99@gmail.com>
2022-09-24 22:19:36 +0700

    chore (Button): handle style & behavior for disabled state

--- src/components/atoms/Button/Button.jsx
+++ src/components/atoms/Button/Button.jsx
@@ -1,18 +1,35 @@
 import classNames from 'classnames';
 
-export const Button = ({ className, type, variant, leftIcon, rightIcon, label, children, ...props }) => {
+export const Button = ({
+	className,
+	type,
+	variant,
+	leftIcon,
+	rightIcon,
+	label,
+	children,
+	disabled,
+	onClick,
+	...props
+}) => {
 	const variantClassName = classNames({
-		'bg-blue-700 hover:bg-blue-600 text-white rounded-md': variant === 'primary',
-		'bg-red-700 hover:bg-red-600 text-white rounded-md': variant === 'danger',
-		'bg-yellow-700 hover:bg-yellow-600 text-white rounded-md': variant === 'warning',
-		'bg-green-700 hover:bg-green-600 text-white rounded-md': variant === 'success',
-		'bg-transparent': variant === 'transparent',
+		'bg-blue-700 hover:bg-blue-600 text-white rounded-md': variant === 'primary' && !disabled,
+		'bg-red-700 hover:bg-red-600 text-white rounded-md': variant === 'danger' && !disabled,
+		'bg-yellow-700 hover:bg-yellow-600 text-white rounded-md': variant === 'warning' && !disabled,
+		'bg-green-700 hover:bg-green-600 text-white rounded-md': variant === 'success' && !disabled,
+		'bg-transparent': variant === 'transparent' && !disabled,
+		'bg-blue-400 text-white rounded-md': variant === 'primary' && disabled,
+		'bg-red-400 text-white rounded-md': variant === 'danger' && disabled,
+		'bg-yellow-400 text-white rounded-md': variant === 'warning' && disabled,
+		'bg-green-400 text-white rounded-md': variant === 'success' && disabled,
+		'text-gray-400': variant === 'transparent' && disabled,
 	});
 
 	return (
 		<button
 			type={type}
 			className={`flex items-center transition-all overflow-hidden ${variantClassName} ${className}`}
+			onClick={!disabled && onClick}
 			{...props}
 		>
 			{leftIcon && <span>{leftIcon}</span>}
--- src/components/atoms/Modal/Modal.jsx
+++ src/components/atoms/Modal/Modal.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Button } from '@/components/atoms';
+import { BiLoaderAlt } from 'react-icons/bi';
 
 export const Modal = ({ title, description, isSubmitting, submitButtonText, onClose, onSubmit, children }) => {
 	return (
 		<div className="fixed z-10 left-0 top-0 w-screen h-screen flex items-center">
-			<div className="absolute left-0 top-0 w-full h-full bg-black bg-opacity-50" onClick={onClose} />
+			<div className="absolute left-0 top-0 w-full h-full bg-black bg-opacity-50" onClick={!isSubmitting && onClose} />
 			<div className="relative w-full max-w-screen-sm mx-auto bg-white shadow-md rounded-md">
 				<div className="p-5 space-y-2">
 					<div className="font-semibold text-xl">{title}</div>
@@ -22,8 +23,14 @@ export const Modal = ({ title, description, isSubmitting, submitButtonText, onCl
 						Cancel
 					</Button>
 
-					<Button className="px-5 py-2" disabled={isSubmitting} variant="primary" onClick={onSubmit}>
-						{submitButtonText}
+					<Button
+						className="px-5 py-2 flex items-center space-x-2"
+						disabled={isSubmitting}
+						variant="primary"
+						onClick={onSubmit}
+						leftIcon={isSubmitting && <BiLoaderAlt className="animate-spin" />}
+					>
+						<span>{submitButtonText}</span>
 					</Button>
 				</div>
 			</div>
